c77958c4f30d2c1ca1a1e1c57050635b9d199549
Bruce chen <cqdeyouxiang@163.com>
2015-08-31 21:25:13 +0800

    Initial Commit

---
+++ MediatorPatter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MediatorPatter
+//
+//  Created by Bruce on 15/8/31.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MediatorPatter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MediatorPatter
+//
+//  Created by Bruce on 15/8/31.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
